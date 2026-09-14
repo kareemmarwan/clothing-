6da366df2b69df7cb8410ca608bf47b7896a5368
Kareem Marwan <kareemkemoo23@gmail.com>
2026-09-14 11:18:42 +0300

    Fix formatting in password generation script

--- scripts/generate-password.js
+++ scripts/generate-password.js
@@ -5,7 +5,7 @@ const password = process.argv[2] || 'admin123'
 bcrypt.hash(password, 10, (err, hash) => {
   if (err) {
     console.error('Error generating hash:', err)
-    process.exit(1)
+    process.exit(1)  
   }
   console.log('Password:', password)
   console.log('Hash:', hash)
